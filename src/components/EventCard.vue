bf5b5dd113f12c2a0c81b002fddcd7fcdfad30ab
Matias Moglia <matimoglia951@gmail.com>
2024-05-26 16:31:16 -0300

    EventCard

---
+++ src/components/EventCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="event-card">
+    <span>@{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  //props: {
+  //  msg: String,
+  //},
+  data() {
+    return {
+      event: {
+        id: 1,
+        title: 'Event Title',
+        description: 'Event Description',
+        location: 'Event Location',
+        date: 'Event Date',
+        time: 'Event Time',
+      },
+    }
+  },
+}
+</script>
+<style scoped>
+.event-card {
+  padding: 20 px;
+  width: 250 px;
+  cursor: pointer;
+  border: 1px solid black;
+  margin-top: 18px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <EventCard />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from '@/components/EventCard.vue'
 
 export default {
-  name: "HomeView",
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Home',
   components: {
-    HelloWorld,
+    EventCard,
   },
-};
+}
 </script>
